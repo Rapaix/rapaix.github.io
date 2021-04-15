15b5d7a56f4744ca1f08b053aec7112159dfe254
Rafael Paixão <rafaelpx8812@gmail.com>
2021-04-14 23:49:57 -0300

    commi inicial

---
+++ case/js/tagueamento.js
@@ -0,0 +1,131 @@
+// Preencha este arquivo com qualquer código que você necessite para realizar a
+// coleta, desde a biblioteca analytics.js, gtag.js ou o snippet do Google Tag 
+// Manager. No último caso, não é necessário implementar a tag <noscript>.
+// O ambiente dispõe da jQuery 3.5.1, então caso deseje, poderá utilizá-la
+// para fazer a sua coleta.
+// Caso tenha alguma dúvida sobre o case, não hesite em entrar em contato.
+
+console.log(ga)
+
+let menuListaContato = document.querySelectorAll('.menu-lista-contato');
+let menuListaDownload = document.querySelectorAll('.menu-lista-download');
+let menuListaAnalise = document.querySelectorAll('.menu-lista-analise');
+let menuListaSobre = document.querySelectorAll('.menu-lista-sobre')
+let menuListaIndex = document.querySelectorAll('.menu-lista-index')
+let analiseBtns = document.querySelectorAll('.cards-montadoras')
+
+// console.log(analiseBtns.getAttribute('data-id'));
+
+
+analiseBtns.forEach( function(btn){
+    console.log(btn.dataset.name)
+    btn.addEventListener('click', function(event){
+        ga('send', 'event',{
+            eventAction: 'ver_mais',
+            eventCategory: 'analise',
+            eventLabel: btn.getAttribute('data-name')
+        }
+
+        )
+    });
+});
+
+menuListaContato.forEach( function(btn){
+    // console.log(btn)
+    btn.addEventListener('click', function(event){
+        ga('send', 'event',{
+            eventAction: 'entre_em_contato',
+            eventCategory: 'menu',
+            eventLabel: 'link_externo'
+        }
+
+        )
+    });
+});
+
+menuListaDownload.forEach( function(btn){
+    // console.log(btn)
+    btn.addEventListener('click', function(event){
+        ga('send', 'event',{
+            eventAction: 'download_pdf',
+            eventCategory: 'menu',
+            eventLabel: 'download_pdf'
+        }
+
+        )
+    });
+});
+// menuListaAnalise.forEach( function(btn){
+//     // console.log(btn)
+//     btn.addEventListener('click', function(event){
+//         console.log(event.target.href)
+//         ga('send', 'event',{
+//             eventAction: 'dropdown_analise',
+//             eventCategory: 'menu',
+//             eventLabel: 'dropdown_analise"
+//         }
+
+//         )
+//     });
+// });
+// menuListaSobre.forEach( function(btn){
+//     // console.log(btn)
+//     btn.addEventListener('click', function(event){
+//         console.log(event.target.href)
+//         ga('send', 'event',{
+//             eventAction: 'dropdown_sobre',
+//             eventCategory: 'menu',
+//             eventLabel: "dropdown_sobre"
+//         }
+
+//         )
+//     });
+// });
+// menuListaIndex.forEach( function(btn){
+//     // console.log(btn)
+//     btn.addEventListener('click', function(event){
+//         console.log(event.target.href)
+//         ga('send', 'event',{
+//             eventAction: 'index',
+//             eventCategory: 'menu',
+//             eventLabel: "index"
+//         }
+
+//         )
+//     });
+// });
+
+
+
+// ga('send', {
+//     hitType: 'event',
+//     eventCategory: [Category],
+//     eventAction: [Action],
+//     eventLabel: [Label],
+//     eventValue: [Value],
+//     nonInteractive: [True | False]
+//   });
+
+function pegarInput(){
+    let id = document.getElementById("nome");
+    let email = document.getElementById("email");
+    let telefone = document.getElementById("telefone");
+    let aceito = document.getElementById("aceito");
+    let arr =[id,email, telefone, aceito]
+    console.log(arr);
+    for(i=0; i < arr.lenght;i++){
+        console.log(arr[i].value);
+    }
+    
+    let campos = id +" "+ email +" " +  telefone +" "+ aceito
+    console.log(campos)
+    ga('send', 'event',{
+                    eventAction: 'contato',
+                    eventCategory: campos,
+                    eventLabel: "preencheu"
+                }
+        
+                )
+    
+
+}
